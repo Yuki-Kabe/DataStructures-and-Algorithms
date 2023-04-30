b114c63fbaa45ec8732890fcf4ab62f267735e0f
okamoto-r <okamoto@ss.cs.osakafu-u.ac.jp>
2023-04-30 12:33:34 +0900

    Add exercise 6-2,3

---
+++ けんちょん本/6章/exercise/6-3.py
@@ -0,0 +1,22 @@
+import bisect
+
+N, M = map(int, input().split())
+P = [0] + [int(input()) for _ in range(N)]
+
+P = [i + j for i in P for j in P]
+P.sort()
+
+ans = 0
+for p in P:
+    if M - p < 0:
+        continue
+    
+    idx = bisect.bisect_right(P, M - p)
+    ans = max(ans, p + P[idx - 1])
+
+print(ans)
+
+
+# Problem: https://atcoder.jp/contests/joi2008ho/tasks/joi2008ho_c
+# Submit: https://atcoder.jp/contests/joi2008ho/submissions/41065348
+# Keyword: 半分全列挙
